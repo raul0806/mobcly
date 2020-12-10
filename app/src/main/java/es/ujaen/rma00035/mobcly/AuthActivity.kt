c66e0f7befe5c3e3abbad42b32bef233040bb20a
Raúl Martinez Atienza <ratienza@ujaen.es>
2020-12-10 11:09:46 +0100

    Crashlitics and authentication added

--- app/src/main/java/es/ujaen/rma00035/mobcly/AuthActivity.kt
+++ app/src/main/java/es/ujaen/rma00035/mobcly/AuthActivity.kt
@@ -1,38 +1,120 @@
 package es.ujaen.rma00035.mobcly
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN_OK = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        setup()
+        session()
     }
-    private fun setup(){
-        singUpButton.setOnClickListener{
-            if (editTextEmailAddress.text.isNotEmpty()&&editTextPassword.text.isNotEmpty()){
+
+    override fun onStart() {
+        super.onStart()
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", "None")
+        if (email != null && provider != null) {
+            authLayout.visibility = View.INVISIBLE
+            showHome()
+        }
+    }
+
+    private fun setup() {
+        title = "Autenticacion"
+        singUpButton.setOnClickListener {
+            if (editTextEmailAddress.text.isNotEmpty() && editTextPassword.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(
                     editTextEmailAddress.text.toString(),
                     editTextPassword.text.toString()
-                ).addOnCompleteListener{
-                    if(it.isSuccessful){
-
-                    }else{
-
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome()
+                    } else {
+                        showAlert()
                     }
                 }
             }
         }
+        logInButton.setOnClickListener {
+            if (editTextEmailAddress.text.isNotEmpty() && editTextPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    editTextEmailAddress.text.toString(),
+                    editTextPassword.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome()
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        googleButton.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN_OK)
+        }
     }
-    private fun showAlert(){
+
+    private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage("Se ha producido un error autenticando al usuario")
-        builder.setPositiveButton("Aceptar",null)
+        builder.setPositiveButton("Aceptar", null)
         val dialog = builder.create()
         dialog.show()//minuto 11
     }
+
+    private fun showHome() {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            //putExtra("clave",valor)
+        }
+        startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+           try{
+            val account = task.getResult(ApiException::class.java)
+            if (account != null) {
+                val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome()
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        } catch (e: ApiException){
+            showAlert()
+        }
+    }}
 }
--- app/src/main/java/es/ujaen/rma00035/mobcly/HomeActivity.kt
+++ app/src/main/java/es/ujaen/rma00035/mobcly/HomeActivity.kt
@@ -1,11 +1,31 @@
 package es.ujaen.rma00035.mobcly
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val bundle = intent.extras
+        //bundle?.get("get")
+        setup()
+        // Guardado de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email","prueba")///cambiar
+        prefs.apply()
+    }
+    private fun setup(){
+        title = "Inicio"
+        logOutButton.setOnClickListener{
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }
